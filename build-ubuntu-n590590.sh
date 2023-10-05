d0ededdedb992a76c184a3dc2ee56e99a7c0075f
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2023-10-05 17:50:48 -0300

    fix: put install plugin in build script for packer

--- build-ubuntu-n590590.sh
+++ build-ubuntu-n590590.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 echo 'Build packer image for ubuntu 22.04'
+
+echo 'packer install plugins'
+packer plugins install -var-file=ubuntu-n590590-var.json ubuntu-n590590.json
+
+echo 'packer init'
+packer init -var-file=ubuntu-n590590-var.json ubuntu-n590590.json
+
+echo 'packer build image'
 packer build -var-file=ubuntu-n590590-var.json ubuntu-n590590.json
